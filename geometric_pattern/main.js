9b18bea51d168b1dd9f040553a2052d2d64bd69b
Rachel Sterneck <rachel.sterneck@gmail.com>
2016-10-26 04:59:22 +0000

    Offset circles in geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,20 +1,32 @@
 var numCircles = 12;
 var circleDiameter;
+var circleRadius;
 
 function setup() {
     createCanvas(480, 600);
     circleDiameter = width/numCircles;
+    circleRadius = circleDiameter/2;
 }
 
 function draw() {
+    var isShifted = false;
+    
     var y = 0;
     while (y <= height){
-    
-    var x = 0;
-    while (x<=width){
-        ellipse(x, y, circleDiameter, circleDiameter)
-        x = x + circleDiameter
+        
+        var x;
+    if (isShifted){
+        x=circleRadius;
     }
-    y = y+circleDiameter
+    else{
+        x=0;
+    }
+    
+    while (x<=width){
+        ellipse(x, y, circleDiameter, circleDiameter);
+        x = x + circleDiameter;
+    }
+    y = y + circleRadius;
+    isShifted = !isShifted;
    }
 }
