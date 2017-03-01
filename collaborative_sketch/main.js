8b4ea91a8f9e770c68fca261ead02fb1f5bcfdd3
Rachel Sterneck <rachel.sterneck@gmail.com>
2017-03-01 06:52:22 +0000

    Create collaborative sketch application

---
+++ collaborative_sketch/main.js
@@ -0,0 +1,61 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwFqai29APy-xw2qQlhNNxevhLMVF7JBQ",
+    authDomain: "collaborative-sketch-87a00.firebaseapp.com",
+    databaseURL: "https://collaborative-sketch-87a00.firebaseio.com",
+    storageBucket: "collaborative-sketch-87a00.appspot.com",
+    messagingSenderId: "410991468357"
+  };
+  firebase.initializeApp(config);
+  
+  var pointsData = firebase.database().ref();
+  var points = [];
+  
+  function setup() {
+        var canvas = createCanvas(400, 400);
+        background(255);
+        fill(0);
+        
+        pointsData.on("child_added", function (point) {
+        points.push(point.val());
+        });
+        
+        pointsData.on("child_removed", function(){
+            points = [];
+        });
+  
+    canvas.mousePressed(drawPoint);
+    canvas.mouseMoved(drawPointIfMousePressed);
+    }
+
+    function draw() {
+        background(255);
+        
+        for (var i = 0; i<points.length; i++) {
+            var point = points[i];
+            ellipse(point.x, point.y, 5, 5);
+        }
+        
+    }
+    
+    function drawPoint() {
+        pointsData.push({x: mouseX, y: mouseY});
+}
+
+    function drawPointIfMousePressed() {
+        if (mouseIsPressed) { 
+            drawPoint();
+        }
+        
+        $("#saveDrawing").on("click", saveDrawing);
+        function saveDrawing() {
+            saveCanvas();
+        }
+        
+        $("#clearDrawing").on("click", clearDrawing);
+
+        function clearDrawing() {
+            pointsData.remove();
+            points = [];
+        }
+    }
